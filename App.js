6191970aa43b0c433a09a317d5a0b9f71a6709f0
navayuvan-sb <ns.navayuvan@gmail.com>
2020-04-18 18:32:21 +0530

    [ADD] Base: React Navigation Added
    React Navigation Added and Configured with all the screens

--- App.js
+++ App.js
@@ -5,21 +5,27 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import Home from './screens/Home/index';
 import Starter1 from './screens/Starter1/index';
 import Starter2 from './screens/Starter1/index';
 import History from './screens/Starter1/index';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <Home />
-      <Starter1 />
-      <Starter2 />
-      <History />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Starter1" component={Starter1} />
+        <Stack.Screen name="Starter2" component={Starter2} />
+        <Stack.Screen name="History" component={History} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
