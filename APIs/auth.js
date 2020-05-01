78b831b91f8f2b03f96a775e750defaedb74ff3c
navayuvan-sb <ns.navayuvan@gmail.com>
2020-05-01 23:45:47 +0530

    [IMP] Auth: Implemented Basic Authentication and Set Root page
    The App will open in Home Page if the user logged in, or it'll open in starter1 page

--- APIs/auth.js
+++ APIs/auth.js
@@ -1,3 +1,14 @@
-export const checkAndLogin = reasonOfUse => {
-  console.log('Blablab');
+import AsyncStorage from '@react-native-community/async-storage';
+
+// Store it in AsyncStorage
+export const checkAndLogin = async reasonOfUse => {
+  try {
+    if (reasonOfUse !== 'unknown' && reasonOfUse.trim !== '') {
+      await AsyncStorage.setItem('@isSession', 'true');
+    } else {
+      console.log('Please enter the reason');
+    }
+  } catch {
+    console.log('Error in Saving the data');
+  }
 };
--- App.js
+++ App.js
@@ -16,28 +16,40 @@ import {createStackNavigator} from '@react-navigation/stack';
 import * as eva from '@eva-design/eva';
 import {ApplicationProvider} from '@ui-kitten/components';
 import {default as theme} from './custom-theme.json';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Stack = createStackNavigator();
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: AsyncStorage.getItem('@isSession'),
+    };
+  }
   render() {
     return (
       <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
         <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen
-              options={{headerShown: false}}
-              name="Starter1"
-              component={Starter1}
-            />
-            <Stack.Screen
-              options={{headerShown: false}}
-              name="Starter2"
-              component={Starter2}
-            />
-            <Stack.Screen name="History" component={History} />
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
+          {this.state.isLoggedIn === 'true' ? (
+            <Stack.Navigator>
+              <Stack.Screen
+                options={{headerShown: false}}
+                name="Starter1"
+                component={Starter1}
+              />
+              <Stack.Screen
+                options={{headerShown: false}}
+                name="Starter2"
+                component={Starter2}
+              />
+            </Stack.Navigator>
+          ) : (
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="History" component={History} />
+            </Stack.Navigator>
+          )}
         </NavigationContainer>
       </ApplicationProvider>
     );
